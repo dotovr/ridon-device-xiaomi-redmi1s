58425efb693007ffdefc65ca47bbba0b2f3ed5b3
Mahyuddin Idram Ahmad <yudi.al@gmail.com>
2015-01-10 14:11:22 +0700

    ridon-5.0

--- cm.mk
+++ cm.mk
@@ -9,6 +9,6 @@ $(call inherit-product, vendor/ridon/config/common_full_phone.mk)
 $(call inherit-product, device/xiaomi/armani/full_armani.mk)
 
 PRODUCT_RELEASE_NAME := Redmi 1S
-PRODUCT_NAME := cm_armani
+PRODUCT_NAME := ridon_armani
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+PRODUCT_GMS_CLIENTID_BASE := ridon-xiaomi
